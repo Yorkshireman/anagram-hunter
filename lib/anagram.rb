e753376c39c82e9abcde0e272ec199e47492a086
Andrew Stelmach <andrew_stelmach@hotmail.com>
2015-09-04 13:49:45 +0100

    first challenge complete

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -5,6 +5,12 @@ class AnagramHunter
   end
 
   def call(word)
+    word_list = ["try","how","eat","far","act","see","hit","lie","key","tac","sit","its","tea"]
+    result = []
+    word_list.each do |word_from_list|
+      result << word_from_list if word.chars.sort.join == word_from_list.chars.sort.join
+    end
+    result
   end
 
 end
